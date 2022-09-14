9cefbed4839a0606615f2133bbc7020146ae0fd0
Guilherme Souza <grsouza@pm.me>
2022-09-14 07:20:14 -0300

    Add BSContentViewController

--- Sources/UIKitHelpers/BSViewController.swift
+++ Sources/UIKitHelpers/BSViewController.swift
@@ -11,4 +11,25 @@ open class BSViewController: UIViewController {
     fatalError("init(coder:) has not been implemented")
   }
 }
+
+/// A generic view controller that displays a custom `Content` view as its root view.
+///
+/// Example:
+/// ```
+/// let customView = MyCustomView()
+/// let viewController = BSContentViewController(content: customView)
+/// navigationController.push(viewController, animated: true)
+/// ```
+open class BSContentViewController<Content: UIView>: BSViewController {
+  let content: Content
+
+  public init(content: Content) {
+    self.content = content
+    super.init()
+  }
+
+  open override func loadView() {
+    view = content
+  }
+}
 #endif
