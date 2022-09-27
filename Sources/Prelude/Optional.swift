cad2fe4c03fd132ecb880013bea9c08430373cf3
Guilherme Souza <grsouza@pm.me>
2022-09-27 05:23:45 -0300

    Add required method for Optional

---
+++ Sources/Prelude/Optional.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+extension Optional {
+  /// Default error for when a required value isn't found.
+  public struct RequiredValueError: Error {
+    public init() {}
+  }
+
+  /// Unwrap Optional value or throw an error if none is found.
+  /// - Parameter error: Error to throw when none value is found.
+  /// - Returns: The wrapped value.
+  public func required(orThrow error: @autoclosure () -> Error = RequiredValueError()) throws -> Wrapped {
+    guard let value = self else {
+      throw error()
+    }
+
+    return value
+  }
+}
